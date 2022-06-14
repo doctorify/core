8d8dd02c9a372a88ba80a68c1650cd0a08f6f48e
MadaraUchiha-314 <rohithr31@gmail.com>
2022-06-14 18:54:04 +0530

    fix: small changes

--- src/main/java/doctorify/core/doctor/DoctorRepository.java
+++ src/main/java/doctorify/core/doctor/DoctorRepository.java
@@ -2,8 +2,5 @@ package doctorify.core.doctor;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import javax.print.Doc;
-import java.util.List;
-
 public interface DoctorRepository extends MongoRepository<Doctor, String> {
 }
